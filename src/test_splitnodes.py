b81d84839405deec6d3ad5858dcedb34234ef9cd
harry <harry_emmett@outlook.com>
2024-11-06 16:16:18 +0000

    feat: add split string function

--- src/test_splitnodes.py
+++ src/test_splitnodes.py
@@ -7,11 +7,11 @@ from textnode import TextNode, TextType
 
 class TestSplitNodes(unittest.TestCase):
     def test_delim_bold(self):
-        node = TextNode("This is text with a **bolded** word", TextType.TEXT)
+        node = TextNode("hello a **bolded** word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
         self.assertListEqual(
             [
-                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("hello a ", TextType.TEXT),
                 TextNode("bolded", TextType.BOLD),
                 TextNode(" word", TextType.TEXT),
             ],
